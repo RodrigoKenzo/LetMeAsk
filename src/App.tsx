6aa0de92165f687e620deb1e7af671e4606be633
SquaredOnion <rokenzo88.rk@gmail.com>
2021-06-23 21:30:52 -0300

    Lesson 2 and 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import { Teste } from './components/teste';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import { Home } from './pages/Home'
+import { NewRoom } from './pages/NewRoom'
+import { Room } from './pages/Room'
+
+import { AuthContextProvider } from './contexts/AuthContext'
 
 function App() {
+  
+
   return (
-    <div>
-      <Teste />
-    </div>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+        </Switch>
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
